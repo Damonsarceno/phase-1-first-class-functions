c1f99615c879432feb3d394c8b01f978eef6056d
Damon sarceno <Damonsarceno@gmail.com>
2023-05-28 15:49:14 -0400

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+function receivesAFunction(callback) {
+    callback();
+    
+}
+function returnsANamedFunction() {
+    return function spy() {}
+}
+function returnsAnAnonymousFunction() {
+    return  function(){}
+    }
